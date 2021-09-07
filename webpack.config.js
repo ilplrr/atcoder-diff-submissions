11256d640d6d2c060b1437233cadf183fe0f953d
ilplrr <ilplrr193@gmail.com>
2021-09-07 21:21:15 +0900

    UserScript のバナー追加とパッケージ名修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,19 @@
 const path = require('path');
+const webpack = require('webpack');
+const packageJson = require('./package.json');
+
+const userScriptBanner = `
+// ==UserScript==
+// @name         ${packageJson.name}
+// @namespace    xxx
+// @version      ${packageJson.version}
+// @description  ${packageJson.description}
+// @author       ${packageJson.author}
+// @license      ${packageJson.license}
+// @match        https://atcoder.jp/contests/*/submissions*
+// @match        https://atcoder.jp/contests/*/submissions/me*
+// @grant        none
+// ==/UserScript==`.trim();
 
 module.exports = {
   mode: 'production',
@@ -12,15 +27,21 @@ module.exports = {
       {
         test: /\.css/,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: { url: false }
           }
         ]
       }
     ]
   },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: userScriptBanner,
+      raw: true
+    })
+  ],
   optimization: {
     minimize: false
   }
